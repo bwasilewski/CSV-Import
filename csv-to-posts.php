9e7d97fab121b3f86204c4ac1f3e44315caf2855
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2020-09-28 22:23:18 -0500

    first commit

---
+++ csv-to-posts.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package CSV to Posts
+ */
+
+/*
+Plugin Name: CSV to Posts
+Plugin URI: https://github.com/bwasilewski/CSV-to-Posts
+Description: Import content from a CSV file into WordPress in the form of posts
+Version: 1.0.0
+Author: Ben Wasilewski <benwasilewski@gmail.com>
+Author URI: https://benwas.com
+License: GNU General Public License v3.0
+Text Domain: csv-to-posts
+ */
+
